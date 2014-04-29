bbbe3563d6437b8b6806a8b512688828a41cabe4
zcarioca <rchargel@zcarioca.net>
2014-04-29 06:26:40 -0400

    Updating root

--- zilch/app_server.go
+++ zilch/app_server.go
@@ -12,7 +12,9 @@ func StartServer(resourceDir, port string) {
 
 	zcc := ZipCodeController{database}
 	pc := PngController{database}
+	sc := StaticController{}
 
+	web.Get("/", sc.RenderRoot)
 	web.Get("/query\\.?(.*)", zcc.Query)
 	web.Post("/query\\.?(.*)", zcc.Query)
 	web.Get("/distribution.jso?n?", zcc.GetDistribution)
@@ -20,6 +22,9 @@ func StartServer(resourceDir, port string) {
 	web.Get("/countries.jso?n?", zcc.GetCountries)
 	web.Post("/countries.jso?n?", zcc.GetCountries)
 	web.Get("/map_(\\d*)\\.png", pc.RenderImage)
+	web.Get("/images/(.*)", sc.RenderImages)
+	web.Get("/js/(.*)", sc.RenderJs)
+	web.Get("/(.*)", sc.RenderHtml)
 
 	fmt.Printf("Server started on port %v in %v\n", port, time.Since(start))
 	web.Run("0.0.0.0:" + port)
--- zilch/static_controller.go
+++ zilch/static_controller.go
@@ -1,1 +1,40 @@
 package zilch
+
+import (
+	"bufio"
+	"github.com/hoisie/web"
+	"io"
+	"os"
+	"strings"
+)
+
+type StaticController struct {
+}
+
+func (c StaticController) RenderRoot(ctx *web.Context) {
+	c.RenderHtml(ctx, "root.html")
+}
+
+func (c StaticController) RenderHtml(ctx *web.Context, page string) {
+	c.renderContent(ctx, "text/html", "./web/"+page)
+}
+
+func (c StaticController) RenderImages(ctx *web.Context, image string) {
+	ext := image[strings.Index(image, ".")+1:]
+	c.renderContent(ctx, "image/"+ext, "./web/images/"+image)
+}
+
+func (c StaticController) RenderJs(ctx *web.Context, jsFile string) {
+	c.renderContent(ctx, "application/javascript", "./web/js/"+jsFile)
+}
+
+func (c StaticController) renderContent(ctx *web.Context, contentType, filepath string) {
+	if file, err := os.Open(filepath); err != nil {
+		w := ResponseWriter{ctx, "HTML"}
+		w.SendError(err)
+	} else {
+		ctx.ContentType(contentType + "; charset=utf-8")
+		reader := bufio.NewReader(file)
+		io.Copy(ctx.ResponseWriter, reader)
+	}
+}
