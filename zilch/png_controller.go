2244d3f200ba677b698962e422d4464863d5d62d
Rafael Chargel <rchargel@chariotsolutions.com>
2014-05-05 10:27:51 -0400

    Added bg image for png

--- zilch/png_controller.go
+++ zilch/png_controller.go
@@ -3,10 +3,13 @@ package zilch
 import (
 	"errors"
 	"github.com/hoisie/web"
+	"github.com/nfnt/resize"
 	"image"
 	"image/color"
 	"image/draw"
+	"image/jpeg"
 	"image/png"
+	"os"
 	"strconv"
 )
 
@@ -18,15 +21,12 @@ func (c PngController) RenderImage(ctx *web.Context, scale string) {
 	int64_scale, _ := strconv.ParseInt(scale, 10, 32)
 	int_scale := int(int64_scale)
 
-	if int_scale < 1 || int_scale > 20 {
+	img, err := c.getBackgroundImage(int_scale)
+	if err != nil {
 		rw := ResponseWriter{ctx, "JSON"}
-		rw.SendError(errors.New("Image size is either too large or too small"))
+		rw.SendError(err)
 		return
 	}
-	img := image.NewRGBA(image.Rect(0, 0, 180*int_scale, 90*int_scale))
-	bgcolor := color.RGBA{0, 0, 87, 255}
-
-	draw.Draw(img, img.Bounds(), &image.Uniform{bgcolor}, image.ZP, draw.Src)
 
 	for _, cim := range c.database.CountryIndexMap {
 		for _, entry := range cim.Entries {
@@ -44,4 +44,26 @@ func (c PngController) drawPoint(img *image.RGBA, lat, lng, scale float32) {
 	p := image.Rect(x-1, y-1, x+1, y+1)
 	zipColor := color.RGBA{255, 255, 255, 255}
 	draw.Draw(img, p, &image.Uniform{zipColor}, image.ZP, draw.Src)
+}
+
+func (c PngController) getBackgroundImage(scale int) (*image.RGBA, error) {
+	if scale < 1 || scale > 20 {
+		return nil, errors.New("Image is too large or too small")
+	}
+	file, err := os.Open("./web/images/world-big-2-grey.jpg")
+	defer file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+	world, err := jpeg.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	world = resize.Resize(uint(180*scale), uint(90*scale), world, resize.Bilinear)
+	img := image.NewRGBA(world.Bounds())
+	draw.Draw(img, img.Bounds(), world, image.ZP, draw.Src)
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 87, 87}}, image.ZP, draw.Over)
+	return img, nil
 }
