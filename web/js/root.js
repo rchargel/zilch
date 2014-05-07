df6a1f32d6aa6cf08c62843f623c8814fe5749e9
Rafael Chargel <rchargel@chariotsolutions.com>
2014-05-07 13:45:45 -0400

    Update root with stats on countries

--- web/js/root.js
+++ web/js/root.js
@@ -4,12 +4,15 @@ angular.module("zilch", []).controller("RootController", function($http, $scope)
 
 	$scope.init = function() {
 		$http.get("/countries.json").success(function(countries) {
-			var list = [], t = 0;
+			var list = [], t = 0, countryTotal = 0;
 			for (var i = 0; i < countries.length; i++) {
-				list.push(countries[i]); 
+				countryTotal = 0;
 				for (var j = 0; j < countries[i].States.length; j++) {
 					t += countries[i].States[j].ZipCodes
+					countryTotal += countries[i].States[j].ZipCodes
 				}
+				countries[i].Total = countryTotal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+				list.push(countries[i]); 
 			}
 			$scope.countries = list;
 			$scope.total = t.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
